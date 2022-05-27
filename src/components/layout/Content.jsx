61fc1fe91549835fa3da520a3951bbced0f8a64f
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-05-27 11:40:39 -0300

    page not found

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -4,6 +4,7 @@ import About from "../../views/examples/About";
 import Home from "../../views/examples/Home";
 import { Routes, Route } from "react-router-dom";
 import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = (props) => {
   return (
@@ -12,6 +13,7 @@ const Content = (props) => {
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/param/:id" element={<Param />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </main>
   );
